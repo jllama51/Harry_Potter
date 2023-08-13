260707a7c2ad409fa933035cf069276286a2a8d3
jllama51 <142173189+jllama51@users.noreply.github.com>
2023-08-13 13:48:50 -0600

    Update harry_potter.js

--- harry_potter.js
+++ harry_potter.js
@@ -1,2 +1,102 @@
 require('dotenv').config();
 const { Voice } = require('@signalwire/realtime-api')
+
+const projectId = process.env.SIGNALWIRE_PROJECT_ID;
+const token = process.env.SIGNALWIRE_TOKEN;
+const verifiedNumber = process.env.VERIFIED_NUMBER;
+
+const client = new Voice.Client({
+    project: projectId,
+    token: token,
+    contexts: ["ivr"],
+  });
+
+  console.log("started");
+
+  client.on("call.received", async (call) => {
+    console.log("Got call", call.from, call.to);
+
+    try {
+        await call.answer();
+        console.log("Inbound call answered");
+      } catch (error) {
+        console.error("Error answering inbound call", error);
+      }
+
+      const playlist = new Voice.Playlist({ volume: 1.0 })
+      .add(
+        Voice.Playlist.Audio({
+          url: "https://www.mediacollege.com/downloads/sound-effects/nature/forest/forest-bright_01.wav",
+        })
+      )
+      .add(
+        Voice.Playlist.TTS({
+          text: "Thank you for calling the Hogwarts school of Witchcraft and Wizardry.",
+        })
+      )
+      .add(Voice.Playlist.Silence({ duration: 1 }))
+      .add(
+        Voice.Playlist.TTS({
+          text: "For Ollivanders wand sales shop press 1. For Wichcraft and wizard spell support appointment line press 2. To speak with a representative about student housing press 3. To hear all options again press 0",
+        })
+      );
+
+      await promptCallOptions(call, playlist);
+    });
+
+    const promptCallOptions = async (call, playlist) => {
+        try {
+          const initialPrompt = await call.prompt({
+            playlist: playlist,
+            digits: {
+              max: 1,
+              digitTimeout: 2,
+            },
+          });
+
+          const { digits } = await initialPrompt.waitForResult();
+
+          switch (digits) {
+            case "1":
+              const playback = await call.playTTS({
+                text: "You have reached Ollivanders wand sales shop. Our hours of operations vary from day to day. We are currently closed to new wand selections due to inventory shortages. Please try calling us back on another day. God Speed!",
+              });
+              await playback.waitForEnded();
+              call.hangup();
+              break;
+            case "2":
+              call.on("prompt.ended", (p) => {
+                console.log(p.id, p.text);
+                call.hangup();
+              });
+
+              const spellSupportPrompt = await call.prompt({
+                playlist: new Voice.Playlist().add(
+                  Voice.Playlist.TTS({
+                    text: "You have reached the Witchcraft and Wizard spell support appointment line. Please say your name and number, reason for calling, and requested time of appointment.",
+                  })
+                  ),
+                speech: {
+                  endSilenceTimeout: 1,
+                  speechTimeout: 60,
+                  language: "en-US",
+                },
+              });
+              break;
+            case "0":
+              await promptCallOptions(call, playlist);
+              break;
+            case "3":
+              const peer = await call.connectPhone({
+                from: verifiedNumber,
+                to: call.from,
+                timeout: 30,
+              });
+              break;
+            default:
+            // Handle other cases or leave it empty if not needed
+          }
+        } catch (error) {
+            console.error("Error during promptCallOptions:", error);
+          }
+        };
